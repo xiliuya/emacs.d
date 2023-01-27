ed2da5975b3a4177c2c925940ab8b2ad9fc1efe3
xiliuya <xiliuya@aliyun.com>
2023-01-27 14:39:58 +0800

    * 屏蔽 pyim v 字符触发的特殊操作

--- templates/template.py
+++ templates/template.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# * filename   : `(buffer-name)`
+# * created at : `(format-time-string "%F %T")`
+# * Author     : `user-full-name` <`user-mail-address`>
